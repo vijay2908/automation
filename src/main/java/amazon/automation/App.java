cc4852f25ce0996d05bbc013c05e179019ce3252
sankar <vijaysankar.asak@gmail.com>
2022-12-26 11:02:09 +0530

    Initial commit

---
+++ src/main/java/amazon/automation/App.java
@@ -0,0 +1,13 @@
+package amazon.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
